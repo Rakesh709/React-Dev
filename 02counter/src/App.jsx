1e317f6768ad723a0ffa2146294755c874389803
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-10-24 00:56:29 +0530

    Counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter,setCounter] =useState(15)
+
+  //let counter =15
+
+  const addValue = ()=> {
+    //console.log("clicked",Math.random());
+    //console.log("clicked",counter);
+    if(counter==20){
+      setCounter(counter=20)
+    }else{
+      setCounter(counter+1) 
+    }
+    
+  }
+
+
+  const removeValue = ()=>{
+    if(counter==0){
+      setCounter(counter=0)
+    }else{
+      setCounter(counter-1)
+    }
+  }
+
+  return (
+    <>
+     <h1>Chai and react</h1>
+     <h2>Counter value {counter}</h2>
+
+     <button
+     onClick={addValue}
+     >Add Value {counter}</button>
+     <br />
+     <button
+     onClick={removeValue}
+     >remove Value {counter}</button>
+     <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
