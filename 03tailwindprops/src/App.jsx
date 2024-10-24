db20eeba483e8ff6c3ce07a42c0fa3e3f2b51fbb
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-10-25 00:30:13 +0530

    props Basics

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObjet= {
+    name:"Rakesh",
+    age:24
+  }
+
+  let newArray= [1,2,3]
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded-sm' >Tailwind test</h1>
+      <h1 className='bg-green-400 text-black rounded-sm mb-4' >Tailwind test</h1>
+        
+
+        <Card name="rakesh" someObject={myObjet} myArray={newArray}  publisher="toi"/>
+        <Card name="Prime" publisher="green gold" />
+
     </>
   )
 }
