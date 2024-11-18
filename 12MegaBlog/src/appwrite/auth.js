b976e417544415f168d73ded80e502af32c9ff3a
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-11-18 22:24:27 +0530

    Appwrite AOuth

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -13,12 +13,56 @@ export class AuthService{
             .setProject(conf.appwriteProjectId);
             this.account = new Account(this.client)
     }
+    //2nd method 
+    //call appwrite services make a wrapper
+    async creatAccount({email,password,name}){
+        try {
+           const userAccount=await this.account.create(ID.unique(),email, password,name) 
+
+           if(userAccount){
+            //call another method
+            // return userAccount;
+            return this.login({email,password})
+           }else{
+            return userAccount
+           }
+        } catch (error) {
+            throw error;
+        }
+    }
+    //login
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    //getCurrentUser
+    async getCurrentUser(){
+        try {
+           return await this.account.get(); 
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+            
+        }
+        return null;
+    }
+
+    //logout
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 const authService = new AuthService();
 
 export default AuthService
 
+//authantication service with appwrite 
 
-
-authService
