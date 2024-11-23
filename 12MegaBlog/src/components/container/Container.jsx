f7d7ca3b80c6f88b500e6af39f94e955fa489929
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-11-23 17:58:49 +0530

    components

---
+++ 12MegaBlog/src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Container({ children }) {
+  return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
+}
+
+export default Container;
+
+//container is just a box like from app.jsx header and footer in between something
+// mostly syling property in container
